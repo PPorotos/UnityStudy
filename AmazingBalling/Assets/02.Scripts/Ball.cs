b48fc205be1c12302f16c2232536b273df0fd02b
PProtos <kjh96682@gmail.com>
2019-04-02 15:40:07 +0900

    유니티 공부

---
+++ AmazingBalling/Assets/02.Scripts/Ball.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public LayerMask whatIsProp;
+
+    public ParticleSystem explosionParticle;
+    public AudioSource explosionAudio;
+
+    public float maxDamage = 100.0f;
+    public float explosionForce = 1000.0f;
+    public float lifeTime = 10.0f;
+    public float explosionRadius = 20.0f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Collider[] colliders =  Physics.OverlapSphere(transform.position, explosionRadius, whatIsProp);
+        explosionParticle.transform.parent = null;
+
+        for (int i = 0; i< colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            targetRigidbody.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+
+            Prop targetProp = colliders[i].GetComponent<Prop>();
+
+            float damage = CalculateDamage(colliders[i].transform.position);
+
+            targetProp.TakeDamage(damage);
+        }
+
+        explosionParticle.Play();
+        explosionAudio.Play();
+
+        Destroy(explosionParticle.gameObject, explosionParticle.duration);
+
+        Destroy(gameObject);
+    }
+
+    private float CalculateDamage(Vector3 targetPositon)
+    {
+        Vector3 explosionToTarget = targetPositon - transform.position;
+
+        float distance = explosionToTarget.magnitude;
+
+        float edgeToCenterDistance = explosionRadius - distance;
+
+        float percentage = edgeToCenterDistance / explosionRadius;
+
+        float damage = maxDamage * percentage;
+
+        damage = Mathf.Max(0, damage);
+
+        return damage;
+    }
+}
---
+++ AmazingBalling/Assets/02.Scripts/Prop.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prop : MonoBehaviour
+{
+    public ParticleSystem explosionParticle;
+
+    public int score = 5;
+
+    public float hp = 10.0f;
+
+    public void TakeDamage(float damage)
+    {
+        hp -= damage;
+
+        if (hp <= 0)
+        {
+            ParticleSystem instance = Instantiate(explosionParticle, transform.position, transform.rotation);
+            Destroy(instance.gameObject, instance.duration);
+
+            AudioSource explosionAudio = instance.GetComponent<AudioSource>();
+            explosionAudio.Play();
+
+            gameObject.SetActive(false);
+
+
+        }
+    }
+}
